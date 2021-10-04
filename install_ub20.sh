6458610970791770a2fb315ae89707e9534a3ac2
Cyber Pool <86299091+cyberpoolorg@users.noreply.github.com>
2021-10-04 17:42:33 +0200

    Update install_ub20.sh

--- install_ub20.sh
+++ install_ub20.sh
@@ -10,119 +10,23 @@
 #                                                                              #
 ################################################################################
 
-sleep 2
+    sleep 2
 
-output() {
-	printf "\E[0;33;40m"
-	echo $1
-	printf "\E[0m"
-}
+    output() {
+	    printf "\E[0;33;40m"
+	    echo $1
+	    printf "\E[0m"
+    }
 
-displayErr() {
-	echo
-	echo $1;
-	echo
-	exit 1;
-}
+    displayErr() {
+	    echo
+	    echo $1;
+	    echo
+	    exit 1;
+    }
 
-echo '
-#!/bin/bash
-
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-RED=$ESC_SEQ"31;01m"
-GREEN=$ESC_SEQ"32;01m"
-YELLOW=$ESC_SEQ"33;01m"
-BLUE=$ESC_SEQ"34;01m"
-MAGENTA=$ESC_SEQ"35;01m"
-CYAN=$ESC_SEQ"36;01m"
-
-function spinner {
- 		local pid=$!
- 		local delay=0.75
- 		local spinstr='|/-\'
- 		while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
- 				local temp=${spinstr#?}
- 				printf " [%c]  " "$spinstr"
- 				local spinstr=$temp${spinstr%"$temp"}
- 				sleep $delay
- 				printf "\b\b\b\b\b\b"
- 		done
- 		printf "    \b\b\b\b"
-}
-
-function hide_output {
-		OUTPUT=$(tempfile)
-		$@ &> $OUTPUT & spinner
-		E=$?
-		if [ $E != 0 ]; then
-		echo
-		echo FAILED: $@
-		echo -----------------------------------------
-		cat $OUTPUT
-		echo -----------------------------------------
-		exit $E
-		fi
-		rm -f $OUTPUT
-}
-
-function apt_get_quiet {
-		DEBIAN_FRONTEND=noninteractive hide_output sudo apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" "$@"
-}
-
-function apt_install {
-		PACKAGES=$@
-		apt_get_quiet install $PACKAGES
-}
-
-function ufw_allow {
-		if [ -z "$DISABLE_FIREWALL" ]; then
-		sudo ufw allow $1 > /dev/null;
-		fi
-}
-
-function restart_service {
-		hide_output sudo service $1 restart
-}
-
-function message_box {
-		dialog --title "$1" --msgbox "$2" 0 0
-}
-
-function input_box {
-		declare -n result=$4
-		declare -n result_code=$4_EXITCODE
-		result=$(dialog --stdout --title "$1" --inputbox "$2" 0 0 "$3")
-		result_code=$?
-}
-
-function input_menu {
-		declare -n result=$4
-		declare -n result_code=$4_EXITCODE
-		local IFS=^$'\n'
-		result=$(dialog --stdout --title "$1" --menu "$2" 0 0 0 $3)
-		result_code=$?
-}
-
-function get_publicip_from_web_service {
-		curl -$1 --fail --silent --max-time 15 icanhazip.com 2>/dev/null
-}
-
-function get_default_privateip {
-		target=8.8.8.8
-
-		if [ "$1" == "6" ]; then target=2001:4860:4860::8888; fi
-		route=$(ip -$1 -o route get $target | grep -v unreachable)
-		address=$(echo $route | sed "s/.* src \([^ ]*\).*/\1/")
-
-		if [[ "$1" == "6" && $address == fe80:* ]]; then
-		interface=$(echo $route | sed "s/.* dev \([^ ]*\).*/\1/")
-		address=$address%$interface
-		fi
-
-		echo $address
-}
-' | sudo -E tee /etc/functions.sh >/dev/null 2>&1
+    wget https://github.com/cyberpoolorg/cybercore/blob/master/extra/functions.sh
+    sudo cp -r functions.sh /etc/
 
     source /etc/functions.sh
 
