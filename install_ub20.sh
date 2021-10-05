097610487b42389350f83b11c0f68a0b2b171d58
Cyber Pool <86299091+cyberpoolorg@users.noreply.github.com>
2021-10-05 09:09:18 +0200

    Update install_ub20.sh

--- install_ub20.sh
+++ install_ub20.sh
@@ -27,20 +27,19 @@
 
     wget -L https://raw.githubusercontent.com/cyberpoolorg/cybercore/master/extra/functions.sh
     sudo cp -r functions.sh /etc/
-
     source /etc/functions.sh
 
     clear
+    
     echo
     echo -e "$GREEN******************************************************************************$COL_RESET"
     echo -e "$GREEN* CyberCore Install Script v0.1                                              *$COL_RESET"
     echo -e "$GREEN* Install CyberCore On Ubuntu 20.04 Running Nginx, Dotnet 5.0 And Postgresql *$COL_RESET"
     echo -e "$GREEN******************************************************************************$COL_RESET"
     echo
-    sleep 3
+    sleep 2
 
 
-    # Update package and Upgrade Ubuntu
     echo
     echo
     echo -e "$CYAN => Updating System And Installing Required Packages $COL_RESET"
@@ -53,25 +52,30 @@
     apt_install apt-transport-https build-essential software-properties-common curl unzip rar htop git
     apt_install libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq3-dev libzmq5 screen cmake
     apt_install certbot python3-certbot-nginx dialog pwgen
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
+
 
     echo
     echo
     echo -e "$RED Generating Random Strong Password For Postgresql !!! $COL_RESET"
     echo
     echo -e "$CYAN => Password Will Be Displayed At The End Of Installtion !!! $COL_RESET"
+    sleep 3
+
     password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
-    # Installing Nginx
+
     echo
     echo
     echo -e "$CYAN => Installing Nginx Server $COL_RESET"
     echo
     sleep 3
-    
+
     if [ -f /usr/sbin/apache2 ]; then
     echo -e "Removing apache..."
     hide_output apt-get -y purge apache2 apache2-*
@@ -83,12 +87,8 @@
     hide_output sudo systemctl enable nginx.service
     hide_output sudo systemctl start cron.service
     hide_output sudo systemctl enable cron.service
-    sleep 5
-    sudo systemctl status nginx | sed -n "1,3p"
-    echo
-    echo -e "$GREEN Done...$COL_RESET"
+    sleep 2
 
-    # Making Nginx a bit hard
     echo 'map $http_user_agent $blockedagent {
     default         0;
     ~*malicious     1;
@@ -98,9 +98,12 @@
     ~*bandit        1;
     }
     ' | sudo -E tee /etc/nginx/blockuseragents.rules >/dev/null 2>&1
+    sudo systemctl status nginx | sed -n "1,3p"
+    sleep 2
+    echo
+    echo -e "$GREEN Done...$COL_RESET"
 
 
-    # Installing Postgres
     echo
     echo
     echo -e "$CYAN => Installing Postgresql $COL_RESET"
@@ -108,13 +111,12 @@
     sleep 3
 
     apt_install postgresql postgresql-contrib
-    sleep 5
     sudo systemctl status postgresql | sed -n "1,3p"
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
 
-    # Installing Dotnet 5
     echo
     echo
     echo -e "$CYAN => Installing Microsoft Dotnet 5.0 $COL_RESET"
@@ -126,7 +128,7 @@
     hide_output sudo apt -y update 
     hide_output sudo apt -y upgrade
     apt_install dotnet-sdk-5.0
-    sleep 5
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
@@ -139,8 +141,8 @@
     sleep 3
     
     apt_install fail2ban
-    sleep 5
     sudo systemctl status fail2ban | sed -n "1,3p"
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
@@ -179,13 +181,12 @@
     hide_output sudo ufw allow 4833/tcp
     hide_output sudo ufw allow 4933/tcp
     hide_output sudo ufw --force enable
-    sleep 5
     sudo systemctl status ufw | sed -n "1,3p"
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
 
-    # Installing CyberCore
     echo
     echo
     echo -e "$CYAN => Installing CyberCore $COL_RESET"
@@ -199,11 +200,11 @@
     chmod -R +x $HOME/cybercore/
     cd $HOME/cybercore/src/Cybercore
     hide_output dotnet publish -c Release --framework net5.0  -o ../../build
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
 
-    # Create DB
     echo
     echo
     echo -e "$CYAN => Creating DB $COL_RESET"
@@ -212,34 +213,32 @@
     echo
     sleep 3
 
+    cd ~
     hide_output sudo -u postgres createuser --superuser cybercore
-    hide_output sudo -u postgres psql -c "alter user cybercore with encrypted password ''"${password}"'';"
+    hide_output sudo -u postgres psql -c "alter user cybercore with encrypted password '$password';"
     hide_output sudo -u postgres createdb cybercore
     hide_output sudo -u postgres psql -c "alter database cybercore owner to cybercore;"
     hide_output sudo -u postgres psql -c "grant all privileges on database cybercore to cybercore;"
-    hide_output PGPASSWORD='"${password}"' psql -d cybercore -U cybercore -h 127.0.0.1 -f $HOME/cybercore/src/Cybercore/Persistence/Postgres/Scripts/createdb.sql
+    hide_output PGPASSWORD=$password psql -d cybercore -U cybercore -h 127.0.0.1 -f $HOME/cybercore/src/Cybercore/Persistence/Postgres/Scripts/createdb.sql
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
 
+
     echo
     echo
-    echo -e "$CYAN => Creating File For Postgresql ! $COL_RESET"
+    echo -e "$CYAN => Creating File For Postgresql !!! $COL_RESET"
     echo
     sleep 3
-
-    #Create PSQL Save
     
     echo '
-    "persistence":{
-      "postgres":{
-        "host": "127.0.0.1",
-        "port": 5432,
-        "user": "cybercore",
-        "password": "'"${password}"'",
-        "database": "cybercore"
-      }
-    }
+    Your Postgresql Credentials
+    ---------------------------
+    user:     cybercore
+    password: '"${password}"'
+    database: cybercore
     ' | sudo -E tee /etc/psql.txt >/dev/null 2>&1
+    sleep 2
     echo
     echo -e "$GREEN Done...$COL_RESET"
     
@@ -258,7 +257,7 @@
     echo -e "$RED Your Postgresql Database is cybercore $COL_RESET"
     echo -e "$RED Your Postgresql Password is "$password" $COL_RESET"
     echo
-    echo -e "$GREEN We Saved It To $HOME/etc/psql.txt $COL_RESET"
+    echo -e "$GREEN We Saved The Postgresql Credentials In /etc/psql.txt $COL_RESET"
     echo
     echo -e "$CYAN Example Config Files Are In $HOME/cybercore/examples/ $COL_RESET"
     echo -e "$CYAN To Start Cybercore run : $HOME/cybercore/build/dotnet Cybercore.dll -c config.json $COL_RESET"
