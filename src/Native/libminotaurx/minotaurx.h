680af62b7cd789060c3b10f0775cc4e5941e3c3d
Cyber Pool <86299091+cyberpoolorg@users.noreply.github.com>
2021-10-07 10:14:08 +0200

    Create minotaurx.h

---
+++ src/Native/libminotaurx/minotaurx.h
@@ -0,0 +1,16 @@
+#ifndef MINOTAURX_H
+#define MINOTAURX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void minotaurx_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
